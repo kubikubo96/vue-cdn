a7a65e0565fc868625f116e41a5059bb06173e3e
NGUYEN TAT TIEN <tien.nguyentat.1@gmail.com>
2021-10-26 20:04:57 +0700

    "mix"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <script src="https://unpkg.com/vue-router@2.0.0/dist/vue-router.js"></script>
     <title>Vuejs</title>
 </head>
 <body>
